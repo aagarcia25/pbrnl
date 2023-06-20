faf64a0e193a5e177630d4f903ea105251eabe6f
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-06-20 16:31:24 -0600

    ajustes

---
+++ public/js/Eje.js
@@ -0,0 +1,214 @@
+const repository = new Repository();
+
+$(document).ready(function() {
+    Funciones_Iniciales();
+    Eventos();
+});
+
+function Funciones_Iniciales() {
+    GetEjes();
+}
+
+function GetEjes() {
+    Func_Cargando();
+    repository.Eje.GetEjes()
+        .then(ResponseGetEjes);
+}
+
+function ResponseGetEjes(response) {
+    if (!response.error) {
+        DestroyDataTable("table");
+        $("#table > tbody > tr").remove();
+        if (response.data.length > 0) {
+            for (var i = 0; i < response.data.length; i++) {
+                $("#table > tbody").append(`
+                    <tr>
+                        <td>${response.data[i].IdEje}</td>
+                        <td>${response.data[i].Descripcion}</td>
+                    </tr>
+                `);
+            }
+        }
+        Func_DataTable("table");
+        swal.close();
+    } else {
+        swal.close();
+        console.log(response.result)
+        Func_Aviso("Anomalía detectada", "Ha ocurrido una anomalía al obtener la información del módulo, favor de intentarlo nuevamente.", "error");
+    }
+}
+
+function Eventos() {
+    SeleccionarTabla();
+    BtnAgregarCatalogo();
+    BtnEditarCatalogo();
+    BtnEliminarCatalogo();
+    BtnGuardarEje();
+}
+
+function SeleccionarTabla() {
+    $('#table tbody').on('click', 'tr', function() {
+        var table = $('#table').DataTable();
+        if ($(this).hasClass('selected')) {
+            $(this).removeClass('selected');
+        } else {
+            table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
+    });
+}
+
+function BtnAgregarCatalogo() {
+    $("#BtnAgregarCatalogo").on("click", function() {
+        $("#modal_accion").text("Agregar");
+        $("#id_eje").val("");
+        $("#descripcion").val("");
+        $("#Modal").modal("show");
+    });
+}
+
+function BtnEditarCatalogo() {
+    $("#BtnEditarCatalogo").on("click", function() {
+        var table = $('#table').DataTable();
+        var index = table.row('.selected').index();
+        var data = table.row(index).data();
+
+        if (!table.rows('.selected').any()) {
+            Func_Aviso("Atención", "Para continuar favor de seleccionar un registro.", "info");
+            return false;
+        }
+        
+        Func_LimpiarModal();
+        $("#id_eje").val(data[0]);
+        $("#descripcion").val(data[1]);
+        $("#modal_accion").text("Editar");
+        $("#Modal").modal("show");
+    });
+}
+
+function BtnEliminarCatalogo() {
+    $('#BtnEliminarCatalogo').click(function() {
+        var table = $('#table').DataTable();
+        var index = table.row('.selected').index();
+        var data = table.row(index).data();
+
+        if (!table.rows('.selected').any()) {
+            Func_Aviso("Atención", "Para continuar favor de seleccionar un registro.", "info");
+            return false;
+        }
+
+        Func_DespliegaConfirmacion("Eliminar " + data[1], "¿Deseas eliminar el registro seleccionado?", "question", "Aceptar", "Cancelar", function(response) {
+            if (response) {
+                var Id = data[0];
+                var request = {
+                    id_eje: Id
+                };
+                Func_Cargando();
+                repository.Eje.DeleteEje(request)
+                    .then(ResponseDeleteEje);
+
+            }
+        });
+    });
+}
+
+function BtnGuardarEje() {
+    $("#form_modal").on("submit", function(event) {
+        event.preventDefault();
+
+        if (Func_Valida()) {
+            var request = {
+                id_Eje: $("#id_eje").val(),
+                descripcion: $("#descripcion").val()
+            };
+
+            Func_DespliegaConfirmacion("Guardar", "¿Deseas guardar la información del eje?", "question", "Aceptar", "Cancelar", function(response) {
+                if (response) {
+                    Func_Cargando();
+                    if ($("#modal_accion").text() == "Editar") {
+                        repository.Eje.EditEje(request)
+                            .then(ResponseEditEje);
+                    } else {
+                        repository.Eje.AddEje(request)
+                            .then(ResponseAddEje);
+                    }
+                }
+            });
+        }
+    });
+}
+
+function ResponseAddEje(response) {
+    if (!response.error) {
+        $("#Modal").modal("hide");
+        Func_LimpiarModal();
+        Func_Toast("success", "Eje agregado.", "El eje fue agregado exitosamente.");
+        GetEjes();
+    } else {
+        console.log(response.result)
+        Func_Aviso("Anomalía detectada", "Ha ocurrido una anomalía al realizar el proceso, favor de intentarlo nuevamente.", "error");
+    }
+}
+
+function ResponseEditEje(response) {
+    if (!response.error) {
+        $("#Modal").modal("hide");
+        Func_LimpiarModal();
+        Func_Toast("success", "Eje editado.", "El eje fue editado exitosamente.");
+        GetEjes();
+    } else {
+        console.log(response.result)
+        Func_Aviso("Anomalía detectada", "Ha ocurrido una anomalía al realizar el proceso, favor de intentarlo nuevamente.", "error");
+    }
+}
+
+function ResponseDeleteEje(response) {
+    if (!response.error) {
+        Func_Toast("success", "Eje eliminado.", "El eje fue eliminado exitosamente.");
+        GetEjes();
+    } else {
+        console.log(response.result)
+        Func_Aviso("Anomalía detectada", "Ha ocurrido una anomalía al realizar el proceso, favor de intentarlo nuevamente.", "error");
+    }
+}
+
+// ======================================================
+// Funciones
+// ======================================================
+
+function Func_Valida() {
+    var id_eje = $("#id_eje");
+    var descripcion = $("#descripcion");
+    var resultado = true;
+
+    if (id_eje.val() == "" || id_eje.val() == null || id_eje.val() == undefined) {
+        resultado = false;
+        id_eje.addClass("is-invalid");
+        id_eje.removeClass("is-valid");
+    } else {
+        id_eje.removeClass("is-invalid");
+        id_eje.addClass("is-valid");
+    }
+
+    if (descripcion.val() == "" || descripcion.val() == null || descripcion.val() == undefined) {
+        resultado = false;
+        descripcion.addClass("is-invalid");
+        descripcion.removeClass("is-valid");
+    } else {
+        descripcion.removeClass("is-invalid");
+        descripcion.addClass("is-valid");
+    }
+
+    if (!resultado) {
+        Func_Aviso("Atención", "Favor de ingresar la información señalada.", "info")
+    }
+
+    return resultado;
+
+}
+
+function Func_LimpiarModal() {
+    $(".form-control").val("");
+    $(".form-control").removeClass("is-invalid");
+    $(".form-control").removeClass("is-valid");
+}
---
+++ public/js/Generales.js
@@ -0,0 +1,146 @@
+// ------------------------------------------------------
+// Funciones generales
+// ------------------------------------------------------
+function Func_DataTable(Tabla, Paginado = true, Info = true, Buscador = true, Order = 0, Order2 = "asc", Menu = 20, Columnas = "_all", Visible = true) {
+    $("#" + Tabla).DataTable({
+        "paging": Paginado,
+        "info": Info,
+        "searching": Buscador,
+        "order": [
+            [Order, Order2]
+        ],
+        "lengthMenu": [
+            [(Menu * 1), (Menu * 2), (Menu * 3), (Menu * 4), (Menu * 5), -1],
+            [(Menu * 1), (Menu * 2), (Menu * 3), (Menu * 4), (Menu * 5), "Todos"]
+        ],
+        fixedHeader: true,
+        "language": {
+            "decimal": "",
+            "emptyTable": "No hay elementos disponibles",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ filas.",
+            "infoEmpty": "Mostrando 0 a 0 de 0 filas.",
+            "infoFiltered": "(filtrado de _MAX_ entradas)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ filas",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "search": "Buscar",
+            "zeroRecords": "No hay coincidencias.",
+            "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "sortAscending": ": activar para ordenar ascendentemente",
+                "sortDescending": ": activar para ordenar descendentemente"
+            }
+        },
+        "sPaginationType": "full_numbers"
+    });
+}
+
+function Func_SearchDataTable(Tabla, Search) {
+    $("#" + Tabla).DataTable().search(Search).draw();
+}
+
+function DestroyDataTable(Tabla) {
+    $("#" + Tabla).DataTable().destroy();
+}
+
+function justNumbers(e) {
+    var keynum = window.event ? window.event.keyCode : e.which;
+    if ((keynum == 8) || (keynum == 46)) {
+        return true;
+    }
+
+    return /\d/.test(String.fromCharCode(keynum));
+}
+
+function Func_Moneda(amount, decimals) {
+    amount += '';
+    Monto = amount;
+    amount = parseFloat(amount.replace(/[^0-9\.]/g, ''));
+    decimals = decimals || 0;
+
+    if (isNaN(amount) || amount === 0)
+        return parseFloat(0).toFixed(decimals);
+
+    amount = '' + amount.toFixed(decimals);
+    var amount_parts = amount.split('.'),
+        regexp = /(\d+)(\d{3})/;
+    while (regexp.test(amount_parts[0]))
+        amount_parts[0] = amount_parts[0].replace(regexp, '$1' + ',' + '$2');
+
+    if (Monto.includes("-")) {
+        return "-" + amount_parts.join('.');
+    } else {
+        return amount_parts.join('.');
+    }
+}
+
+function Func_Moneda(amount, decimals) {
+    Resultado = Func_FormatoMoneda(amount, decimals);
+    return Resultado = "$ " + Resultado;
+}
+
+function Func_DespliegaConfirmacion(title, text, icon, btnConfirm, btnCancel, next) {
+    swal.fire({
+        title: title,
+        text: text,
+        icon: icon,
+        showCancelButton: true,
+        confirmButtonColor: '#cfe1b9',
+        cancelButtonColor: '#adb5bd',
+        cancelButtonText: btnCancel,
+        confirmButtonText: btnConfirm,
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        reverseButtons: true
+    }).then((function(result) {
+        next(result.value)
+    }));
+}
+
+function Func_Aviso(title, text, type) {
+    Swal.fire(
+        title,
+        text,
+        type
+    );
+}
+
+function Func_Cargando(Mensaje = "Cargando...") {
+    swal.fire({
+        title: Mensaje,
+        allowOutsideClick: false,
+        didOpen: function() {
+            swal.showLoading()
+        }
+    });
+}
+
+function Func_Toast(icon, title, text, time = 5000) {
+    $.toast({
+        heading: title,
+        text: text,
+        showHideTransition: 'slide',
+        icon: icon,
+        hideAfter: time,
+        position: 'top-right',
+        bgColor: '#C8B294',
+        loaderBg: '#B58E5A'
+    })
+}
+
+function SetYYYYMMDD_DDMMYYYY(Fecha){
+    var Fechas = Fecha.split("-");
+    return FechaFinal = Fechas[2] + "/" + Fechas[1] + "/" + Fechas[0];
+}
+
+function SetDDMMYYYY_YYYYMMDD(Fecha){
+    var Fechas = Fecha.split("/");
+    return FechaFinal = Fechas[2] + "-" + Fechas[1] + "-" + Fechas[0];
+}
