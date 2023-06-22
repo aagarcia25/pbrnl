57771105b8876fbab4bf3b115b3363c5fb91814e
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-06-22 13:50:58 -0600

    Revert "ajustes"
    This reverts commit e022d58f8c94c99da81caf9647708d865d584865.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'stripe/*',
-        'http://evalua-pbr.nl.gob.mx:81/*'
+        'http://127.0.0.1:8000/*'
     ];
 }
