6744c764c343322be7510b849546452463d24bb5
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-07-03 10:43:02 -0600

    Revert "ajustes"
    This reverts commit 08a76efa41964eaaee2ba8b990ede0776c5edd22.

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     'url' => env('APP_URL', 'http://evalua-pbr.nl.gob.mx:interfaz/'),     
     /*'url' => env('APP_URL',  'http://evalua-pbr.nl.gob.mx:81//'), */    
 
-    'asset_url' => env('ASSET_URL', 'http://evalua-pbr.nl.gob.mx/interfaz/'),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
