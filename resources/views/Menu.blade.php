183c7667be755762c521fde089aa4b8b04b2b8bc
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-07-03 16:44:51 -0600

    Update Menu.blade.php

--- resources/views/Menu.blade.php
+++ resources/views/Menu.blade.php
@@ -4,8 +4,7 @@ $view = "Menu de inicio";
 
 @include('includes._partialHeader')
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet" />
-<link rel="stylesheet" type="text/css" href="/css/MenuInicio.css" />
-
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/MenuInicio.css') }}"  />
 <input type="hidden" id="tipo_usuario" value="@php session('tipo_usuario'); @endphp">
 <input type="hidden" id="admin_evalua" value="@php session('admin_evalua'); @endphp">
 <input type="hidden" id="catalogos_pbr" value="@php session('catalogos_pbr'); @endphp">
@@ -18,7 +17,7 @@ $view = "Menu de inicio";
         <div class="col-lg-4">
             <div class="d-flex align-items-center justify-content-center">
                 <!-- <a href="/Administracion">  -->
-                <a @if (session('admin_evalua') == 1) href="/MenuAdmin" @else href="#" @endif>        
+                <a @if (session('admin_evalua') == 1) href="/public/MenuAdmin" @else href="#" @endif>        
                     <img class="BotonImagen3" 
                     @if (session('admin_evalua') == 1) 
                         src="img/boton admin usuarios.svg" 
@@ -33,7 +32,7 @@ $view = "Menu de inicio";
 
         <div class="col-lg-4">
             <div class="d-flex align-items-center justify-content-center">
-                <a @if (session('catalogos_pbr') == 1) href="/Catalogos" @else href="#" @endif class="d-flex align-items-center justify-content-center">
+                <a @if (session('catalogos_pbr') == 1) href="/public/Catalogos" @else href="#" @endif class="d-flex align-items-center justify-content-center">
                     <img class="BotonImagen1" 
                     @if (session('catalogos_pbr') == 1) 
                         src="img/Catalogos.svg" 
@@ -59,7 +58,7 @@ $view = "Menu de inicio";
         </div>
         <div class="col-lg-4">
             <div class="d-flex align-items-center justify-content-center">
-                <a @if (session('admin_mir') == 1) href="/MenuMIR" @else href="#" @endif class="d-flex align-items-center justify-content-center">
+                <a @if (session('admin_mir') == 1) href="/public/MenuMIR" @else href="#" @endif class="d-flex align-items-center justify-content-center">
                     <img class="BotonImagen4" 
                     @if (session('admin_mir') == 1)
                         src="img/BotonMIR.svg"
