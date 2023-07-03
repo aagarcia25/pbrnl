485a0de42c1f2c01651c0c52b3f2a5fa23e567ca
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-07-03 14:44:51 -0600

    ajustes por contexto

--- config/app.php
+++ config/app.php
@@ -52,10 +52,10 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://evalua-pbr.nl.gob.mx/interfaz/'),     
+    'url' => env('APP_URL', 'http://evalua-pbr.nl.gob.mx/public'),     
     /*'url' => env('APP_URL',  'http://evalua-pbr.nl.gob.mx:81//'), */    
 
-    'asset_url' => env('ASSET_URL', 'http://evalua-pbr.nl.gob.mx/interfaz'),
+    'asset_url' => env('ASSET_URL', 'http://evalua-pbr.nl.gob.mx/public'),
 
     /*
     |--------------------------------------------------------------------------
