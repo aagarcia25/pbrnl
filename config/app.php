41cd17812c41bb96d171ce2dab0ca3d78fa1aefe
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-07-03 10:50:53 -0600

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://evalua-pbr.nl.gob.mx:interfaz/'),     
+    'url' => env('APP_URL', 'http://evalua-pbr.nl.gob.mx/interfaz/'),     
     /*'url' => env('APP_URL',  'http://evalua-pbr.nl.gob.mx:81//'), */    
 
     'asset_url' => env('ASSET_URL', 'http://evalua-pbr.nl.gob.mx/interfaz'),
