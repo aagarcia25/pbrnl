688dcedd326f1c69ce76219861c16db290aaa0eb
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-07-04 17:05:06 -0600

    Revert "ajustes"
    This reverts commit 7e7c02949cb6d19a350b43915c16fb4668cc6bbb.

--- public/vendor/laravel/framework/src/Illuminate/Filesystem/FilesystemAdapter.php
+++ public/vendor/laravel/framework/src/Illuminate/Filesystem/FilesystemAdapter.php
@@ -559,8 +559,8 @@ class FilesystemAdapter implements CloudFilesystemContract
         // If the path contains "storage/public", it probably means the developer is using
         // the default disk to generate the path instead of the "public" disk like they
         // are really supposed to use. We will remove the public from this path here.
-        if (Str::contains($path, '/storage/interfaz/')) {
-            return Str::replaceFirst('/interfaz/', '/', $path);
+        if (Str::contains($path, '/storage/public/')) {
+            return Str::replaceFirst('/public/', '/', $path);
         }
 
         return $path;
