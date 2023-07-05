dbc1e25e13fcc67b98599606a7798da791dc075c
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-07-05 09:11:49 -0600

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'stripe/*',
-        'http://127.0.0.1:8000/*'
+        'http://127.0.0.1:8000/*',
+        'http://evalua-pbr.nl.gob.mx/interfaz/*'
     ];
 }
