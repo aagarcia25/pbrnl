b4a2935bd83321156c82d2934c6fd3b39192c05a
Omar Lopez <darkoware@gmail.com>
2023-08-21 17:46:24 -0600

    arregladas llaves primarias en programatico y componentes

--- database/script-cambios.sql
+++ database/script-cambios.sql
@@ -52,3 +52,53 @@ ALTER TABLE PROGRAMATICO_PI_COMP
 
 -- 11/08/2023: UPDATE REPO PRIVADO
 -- 11/08/2023: NUEVO UPDATE, DESDE OTRA COMPUTADORA
+
+
+-- CORREGIR LAS CLAVES PARA ENLAZAR LOS COMPONENTES CON LAS MIR
+
+ALTER TABLE PROGRAMATICO DROP PRIMARY KEY;
+ALTER TABLE PROGRAMATICO ADD Id INT PRIMARY KEY AUTO_INCREMENT;
+
+ALTER TABLE PROGRAMATICO_COMP DROP PRIMARY KEY;
+ALTER TABLE PROGRAMATICO_COMP ADD Id INT PRIMARY KEY AUTO_INCREMENT;
+
+ALTER TABLE PROGRAMATICO_COMP ADD ProgramaticoId INT;	-- referencia al programa presupuestario
+
+UPDATE PROGRAMATICO_COMP c SET ProgramaticoId = 
+	(SELECT Id FROM PROGRAMATICO AS p 
+		WHERE p.idObjetivoPED = c.idObjetivoPED
+		AND p.idClasificacion = c.idClasificacion
+		AND p.Consecutivo = c.Consecutivo
+		AND p.idSecretaria = c.idSecretaria
+		AND p.ejercicioFiscal = c.ejercicioFiscal
+		)
+
+
+-- agregar al componente el id que estamos generando
+ALTER TABLE COMPONENTE1 ADD ComponenteId INT;
+
+-- actualizar el componente de la mir
+UPDATE COMPONENTE1 c1 
+INNER JOIN
+	PROGRAMATICO_COMP cc
+ON 
+	c1.Componente = cc.descripcioncomponente
+	AND c1.ClasProgramatica = cc.Consecutivo
+SET ComponenteId = cc.Id;
+
+-- agregar a la mir el id del programa presupuestario
+ALTER TABLE MIR_CARATULA ADD ProgramaticoId INT;
+
+UPDATE MIR_CARATULA m
+	INNER JOIN PROGRAMATICO c
+	ON 
+	    m.Consecutivo = c.Consecutivo
+	AND m.EjercicioFiscal = c.EjercicioFiscal
+	AND m.idObjetivo = c.idObjetivoPED
+	AND c.EjercicioFiscal = 2023
+SET m.ProgramaticoId = c.Id
+
+-- -------------------------------------------
+ALTER TABLE COMPONENTE1 DROP PRIMARY KEY;
+ALTER TABLE COMPONENTE1 ADD Id INT PRIMARY KEY AUTO_INCREMENT;
+
