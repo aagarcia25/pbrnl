95269aef4e054ba672d4ed93e6c2b5ea9954e097
omar en gamita <darkoware@gmail.com>
2023-10-04 08:29:49 -0600

    bugfix guardar mir y proposito

--- app/Http/Controllers/MirController.php
+++ app/Http/Controllers/MirController.php
@@ -357,7 +357,11 @@ class MirController extends BaseController
                 ->first();
 
             if (is_null($update_fin)) {
-                return response()->json(array('error' => true, 'result' => "No se ha encontrado la información del fin.", 'code' => 404));
+                $fin = new MirFin();
+                $fin->ClasProgramatica = $request->caratula['consecutivo_caratula'];
+                $fin->EjercicioFiscal = $ef;
+                $fin->ClaveIndicador = $request->caratula['consecutivo_caratula'] . "F";
+                $fin->save();
             }
             
             // Validaciones de fin
@@ -546,7 +550,11 @@ class MirController extends BaseController
                 ->first();
 
             if (is_null($update_proposito)) {
-                return response()->json(array('error' => true, 'result' => "No se ha encontrado la información del proposito.", 'code' => 404));
+                $proposito = new MirProposito();
+                $proposito->ClaveIndicador = $request->caratula['consecutivo_caratula'] . "F";
+                $proposito->ClasProgramatica = $request->caratula['consecutivo_caratula'];
+                $proposito->EjercicioFiscal = $ef;
+                $proposito->save();
             }
 
             // Validaciones de proposito
